11212bc91911687602c10e043cde36e934d86876
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-06-26 15:59:21 +0800

    feat: implement a function to process json and return an object with the data needed

--- src/index.js
+++ src/index.js
@@ -4,29 +4,23 @@ import "./styles.css"
 async function fetchData(url) {
     const response = await fetch(url);
     const json = await response.json();
-
-    console.log(json.latitude);
-
+    
     return json;
 }
 
-function processJSON(url) {
-    // this code is wrong
-    // the return of then() is still another promise
-    // to actually access the value of the async fetchData json promise
-    // we need to access inside .then()
-    const json = fetchData(url).then((resultJson) => resultJson);
-    const latitude = json.latitude;
-    console.log(latitude);
+async function processJSON(url) {
+    const json = await fetchData(url);
+    const tempC = (json.currentConditions.temp - 32) * 5 / 9;
 
-    // correct code
-    fetchData(url).then((resultJson) => {
-        const latitude = resultJson.latitude;
-        console.log(latitude)
-    });
-
+    return {
+        "location": json.address,
+        "tempF": json.currentConditions.temp,
+        "tempC": tempC,
+        "weatherDesc": json.currentConditions.conditions,
+        "icon": json.currentConditions.icon
+    }
 }
 
 const endPt = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/hanoi?key=6NQ57A6JYZH9425ZWCTGC9ZEZ'
 
-processJSON(endPt);
+console.log( await processJSON(endPt) );
